33270dcc97b02d0e96e6540cd477a4c5d03be11d
bodde <tomaso.donini@gmail.com>
2021-12-27 12:45:26 +0100

    Keyboard added as main component

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,20 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-
-    <Key name="C" />
-    <Key name="C#" />
-    <Key name="D" />
+    <Keyboard />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
-import Key from './components/Key.vue';
+import Keyboard from './components/Keyboard.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    Key,
-  },
+    Keyboard,
+  }
 };
 </script>
 
@@ -28,17 +25,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-}
-
-div.app-key {
-  width: 30px;
-  height: 30px;
-  border-color: black;
-  border-style: solid;
-  border-width: 0 1px 1px 1px;
-}
-
-div.app-key:first-child {
-  border-width: 1px;
 }
 </style>
---
+++ src/components/Keyboard.vue
@@ -0,0 +1,50 @@
+<template>
+  <Key v-for="key in keys.a" :name="key.note" />
+  <Key name="C#" />
+  <Key name="D" />
+</template>
+
+<script>
+import Key from './Key.vue';
+
+export default {
+  name: 'Keyboard',
+  components: {
+    Key,
+  },
+  data: function () {
+    return {
+      keys: {
+        a: [
+          { note: 'G#', scale: [] },
+          { note: 'D#', scale: [] },
+          { note: 'A#', scale: [] },
+          { note: 'F', scale: [] },
+          { note: 'C', scale: [] },
+          { note: 'G', scale: [] },
+          { note: 'D', scale: [] },
+          { note: 'A', scale: [] },
+          { note: 'E', scale: [] },
+          { note: 'B', scale: [] },
+          { note: 'F#', scale: [] },
+          { note: 'C#', scale: [] },
+        ],
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+div.app-key {
+  width: 30px;
+  height: 30px;
+  border-color: black;
+  border-style: solid;
+  border-width: 0 1px 1px 1px;
+}
+
+div.app-key:first-child {
+  border-width: 1px;
+}
+</style>
